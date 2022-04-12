76ecdaa0c6aa62ed6d97284060f99a94b895974c
Davi Soares <94925054+DaviSFS21@users.noreply.github.com>
2022-04-12 18:25:33 -0300

    Add files via upload

---
+++ cad_prod.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="opera-gx">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        
+        require("connect.php");
+        
+        $descricao = $_POST['c_descricao'];
+        $estoque = $_POST['c_estoque'];
+        $marca = $_POST['c_marca'];
+        $c_hardware = $_POST['c_hardware'];
+        $termo = $_POST['c_termo'];
+        
+        $sql = "INSERT INTO `item`(`descricao`, `estoque`, `marca`, `c_hardware`, `categoria_livro`,`termo`)
+        VALUES ('$descricao', '$estoque', '$marca', '$hardware', '$categoria_livro', '$termo')";
+
+        $resultado = mysqli_query($conexao, $sql);
+
+        echo "Inserido com sucesso";
+    ?>
+    <p><a href = "index.html">Início</a>
+</body>
+</html>
